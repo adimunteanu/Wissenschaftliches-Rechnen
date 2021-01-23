c083571f1b3c62dd9c0da8f5b713c22e1dc8a7e5
Adrian Munteanu <adimunteanu13@gmail.com>
2021-01-23 11:46:57 +0100

    add praxis 4 Vorgaben

---
+++ wr_praxis_4/main.py
@@ -0,0 +1,108 @@
+import numpy as np
+
+
+####################################################################################################
+# Exercise 1: Interpolation
+
+def lagrange_interpolation(x: np.ndarray, y: np.ndarray) -> (np.poly1d, list):
+    """
+    Generate Lagrange interpolation polynomial.
+
+    Arguments:
+    x: x-values of interpolation points
+    y: y-values of interpolation points
+
+    Return:
+    polynomial: polynomial as np.poly1d object
+    base_functions: list of base polynomials
+    """
+
+    assert (x.size == y.size)
+
+    polynomial = np.poly1d(0)
+    base_functions = []
+
+    # TODO: Generate Lagrange base polynomials and interpolation polynomial
+
+    return polynomial, base_functions
+
+
+
+
+def hermite_cubic_interpolation(x: np.ndarray, y: np.ndarray, yp: np.ndarray) -> list:
+    """
+    Compute hermite cubic interpolation spline
+
+    Arguments:
+    x: x-values of interpolation points
+    y: y-values of interpolation points
+    yp: derivative values of interpolation points
+
+    Returns:
+    spline: list of np.poly1d objects, each interpolating the function between two adjacent points
+    """
+
+    assert (x.size == y.size == yp.size)
+
+    spline = []
+    # TODO compute piecewise interpolating cubic polynomials
+    return spline
+
+
+
+####################################################################################################
+# Exercise 2: Animation
+
+def natural_cubic_interpolation(x: np.ndarray, y: np.ndarray) -> list:
+    """
+    Intepolate the given function using a spline with natural boundary conditions.
+
+    Arguments:
+    x: x-values of interpolation points
+    y: y-values of interpolation points
+
+    Return:
+    spline: list of np.poly1d objects, each interpolating the function between two adjacent points
+    """
+
+    assert (x.size == y.size)
+    # TODO construct linear system with natural boundary conditions
+
+    # TODO solve linear system for the coefficients of the spline
+
+    spline = []
+    # TODO extract local interpolation coefficients from solution
+
+
+    return spline
+
+
+def periodic_cubic_interpolation(x: np.ndarray, y: np.ndarray) -> list:
+    """
+    Interpolate the given function with a cubic spline and periodic boundary conditions.
+
+    Arguments:
+    x: x-values of interpolation points
+    y: y-values of interpolation points
+
+    Return:
+    spline: list of np.poly1d objects, each interpolating the function between two adjacent points
+    """
+
+    assert (x.size == y.size)
+    # TODO: construct linear system with periodic boundary conditions
+
+    # TODO solve linear system for the coefficients of the spline
+
+    spline = []
+    # TODO extract local interpolation coefficients from solution
+
+
+    return spline
+
+
+if __name__ == '__main__':
+
+    print("All requested functions for the assignment have to be implemented in this file and uploaded to the "
+          "server for the grading.\nTo test your implemented functions you can "
+          "implement/run tests in the file tests.py (> python3 -v test.py [Tests.<test_function>]).")
