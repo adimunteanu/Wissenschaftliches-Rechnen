0b538df11a28d6a7ac0e86d2cf6dd0f97dde30ba
Adrian Munteanu <adimunteanu13@gmail.com>
2021-01-23 18:38:19 +0100

    Aufgabe 2 solution

--- wr_praxis_4/main.py
+++ wr_praxis_4/main.py
@@ -90,12 +90,75 @@ def natural_cubic_interpolation(x: np.ndarray, y: np.ndarray) -> list:
 
     assert (x.size == y.size)
     # TODO construct linear system with natural boundary conditions
+    n = x.size
+
+    V = np.zeros((4 * n - 4, 4 * n - 4))
+    f = np.zeros(4 * n - 4)
+
+    for i in range(n):
+        if i == 0:
+            # first row
+            V[0, 0] = 1
+            V[0, 1] = x[i]
+            V[0, 2] = x[i]**2
+            V[0, 3] = x[i]**3
+            # second last row
+            V[4 * n - 6, 2] = 2
+            V[4 * n - 6, 3] = 6 * x[i]
+
+            f[0] = y[0]
+        elif i == n - 1:
+            # third last row
+            V[4 * n - 7, 4 * n - 8] = 1
+            V[4 * n - 7, 4 * n - 7] = x[i]
+            V[4 * n - 7, 4 * n - 6] = x[i]**2
+            V[4 * n - 7, 4 * n - 5] = x[i]**3
+
+            # last row
+            V[4 * n - 5, 4 * n - 6] = 2
+            V[4 * n - 5, 4 * n - 5] = 6 * x[i]
+
+            f[4 * n - 7] = y[n - 1]
+        else:
+            V[4 * (i - 1) + 1, 4 * (i - 1)] = 1
+            V[4 * (i - 1) + 1, 4 * (i - 1) + 1] = x[i]
+            V[4 * (i - 1) + 1, 4 * (i - 1) + 2] = x[i]**2
+            V[4 * (i - 1) + 1, 4 * (i - 1) + 3] = x[i]**3
+
+            V[4 * (i - 1) + 2, 4 * (i - 1)] = 0
+            V[4 * (i - 1) + 2, 4 * (i - 1) + 1] = 1
+            V[4 * (i - 1) + 2, 4 * (i - 1) + 2] = 2 * x[i]
+            V[4 * (i - 1) + 2, 4 * (i - 1) + 3] = 3 * x[i]**2
+            V[4 * (i - 1) + 2, 4 * (i - 1) + 4] = 0
+            V[4 * (i - 1) + 2, 4 * (i - 1) + 5] = -1
+            V[4 * (i - 1) + 2, 4 * (i - 1) + 6] = -2 * x[i]
+            V[4 * (i - 1) + 2, 4 * (i - 1) + 7] = -3 * x[i]**2
+
+            V[4 * (i - 1) + 3, 4 * (i - 1)] = 0
+            V[4 * (i - 1) + 3, 4 * (i - 1) + 1] = 0
+            V[4 * (i - 1) + 3, 4 * (i - 1) + 2] = 2
+            V[4 * (i - 1) + 3, 4 * (i - 1) + 3] = 6 * x[i]
+            V[4 * (i - 1) + 3, 4 * (i - 1) + 4] = 0
+            V[4 * (i - 1) + 3, 4 * (i - 1) + 5] = 0
+            V[4 * (i - 1) + 3, 4 * (i - 1) + 6] = -2
+            V[4 * (i - 1) + 3, 4 * (i - 1) + 7] = -6 * x[i]
+
+            V[4 * (i - 1) + 4, 4 * i] = 1
+            V[4 * (i - 1) + 4, 4 * i + 1] = x[i]
+            V[4 * (i - 1) + 4, 4 * i + 2] = x[i]**2
+            V[4 * (i - 1) + 4, 4 * i + 3] = x[i]**3
+
+            f[4 * (i - 1) + 1] = y[i]
+            f[4 * (i - 1) + 4] = y[i]
+
 
     # TODO solve linear system for the coefficients of the spline
+    c = np.linalg.solve(V, f)
 
     spline = []
     # TODO extract local interpolation coefficients from solution
-
+    for i in range(n - 1):
+        spline.append(np.poly1d(np.flipud(c[4 * i: 4 * i + 4])))
 
     return spline
 
@@ -114,12 +177,91 @@ def periodic_cubic_interpolation(x: np.ndarray, y: np.ndarray) -> list:
 
     assert (x.size == y.size)
     # TODO: construct linear system with periodic boundary conditions
+    n = x.size
+
+    V = np.zeros((4 * n - 4, 4 * n - 4))
+    f = np.zeros(4 * n - 4)
+
+    for i in range(n):
+        if i == 0:
+            # first row
+            V[0, 0] = 1
+            V[0, 1] = x[i]
+            V[0, 2] = x[i] ** 2
+            V[0, 3] = x[i] ** 3
+            # second last row
+            V[4 * n - 6, 0] = 0
+            V[4 * n - 6, 1] = 1
+            V[4 * n - 6, 2] = 2 * x[i]
+            V[4 * n - 6, 3] = 3 * x[i]**2
+
+            # last row
+            V[4 * n - 5, 0] = 0
+            V[4 * n - 5, 1] = 0
+            V[4 * n - 5, 2] = 2
+            V[4 * n - 5, 3] = 6 * x[i]
+
+
+            f[0] = y[0]
+        elif i == n - 1:
+            # third last row
+            V[4 * n - 7, 4 * n - 8] = 1
+            V[4 * n - 7, 4 * n - 7] = x[i]
+            V[4 * n - 7, 4 * n - 6] = x[i]**2
+            V[4 * n - 7, 4 * n - 5] = x[i]**3
+
+            # second last row
+            V[4 * n - 6, 4 * n - 8] = 0
+            V[4 * n - 6, 4 * n - 7] = -1
+            V[4 * n - 6, 4 * n - 6] = -2 * x[i]
+            V[4 * n - 6, 4 * n - 5] = -3 * x[i]**2
+
+            # last row
+            V[4 * n - 5, 4 * n - 8] = 0
+            V[4 * n - 5, 4 * n - 7] = 0
+            V[4 * n - 5, 4 * n - 6] = -2
+            V[4 * n - 5, 4 * n - 5] = -6 * x[i]
+
+            f[4 * n - 7] = y[n - 1]
+        else:
+            V[4 * (i - 1) + 1, 4 * (i - 1)] = 1
+            V[4 * (i - 1) + 1, 4 * (i - 1) + 1] = x[i]
+            V[4 * (i - 1) + 1, 4 * (i - 1) + 2] = x[i] ** 2
+            V[4 * (i - 1) + 1, 4 * (i - 1) + 3] = x[i] ** 3
+
+            V[4 * (i - 1) + 2, 4 * (i - 1)] = 0
+            V[4 * (i - 1) + 2, 4 * (i - 1) + 1] = 1
+            V[4 * (i - 1) + 2, 4 * (i - 1) + 2] = 2 * x[i]
+            V[4 * (i - 1) + 2, 4 * (i - 1) + 3] = 3 * x[i] ** 2
+            V[4 * (i - 1) + 2, 4 * (i - 1) + 4] = 0
+            V[4 * (i - 1) + 2, 4 * (i - 1) + 5] = -1
+            V[4 * (i - 1) + 2, 4 * (i - 1) + 6] = -2 * x[i]
+            V[4 * (i - 1) + 2, 4 * (i - 1) + 7] = -3 * x[i] ** 2
+
+            V[4 * (i - 1) + 3, 4 * (i - 1)] = 0
+            V[4 * (i - 1) + 3, 4 * (i - 1) + 1] = 0
+            V[4 * (i - 1) + 3, 4 * (i - 1) + 2] = 2
+            V[4 * (i - 1) + 3, 4 * (i - 1) + 3] = 6 * x[i]
+            V[4 * (i - 1) + 3, 4 * (i - 1) + 4] = 0
+            V[4 * (i - 1) + 3, 4 * (i - 1) + 5] = 0
+            V[4 * (i - 1) + 3, 4 * (i - 1) + 6] = -2
+            V[4 * (i - 1) + 3, 4 * (i - 1) + 7] = -6 * x[i]
+
+            V[4 * (i - 1) + 4, 4 * i] = 1
+            V[4 * (i - 1) + 4, 4 * i + 1] = x[i]
+            V[4 * (i - 1) + 4, 4 * i + 2] = x[i] ** 2
+            V[4 * (i - 1) + 4, 4 * i + 3] = x[i] ** 3
+
+            f[4 * (i - 1) + 1] = y[i]
+            f[4 * (i - 1) + 4] = y[i]
 
     # TODO solve linear system for the coefficients of the spline
+    c = np.linalg.solve(V, f)
 
     spline = []
     # TODO extract local interpolation coefficients from solution
-
+    for i in range(n - 1):
+        spline.append(np.poly1d(np.flipud(c[4 * i: 4 * i + 4])))
 
     return spline
 
